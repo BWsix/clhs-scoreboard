aee488d61db82a5f6f25674b7680c54fa9a49396
BWsix <itisbw62@gmail.com>
2022-04-03 19:34:16 +0800

    info card

---
+++ src/components/LoginPage/Updates/Updates.CalendarAndTheming.tsx
@@ -0,0 +1,9 @@
+import { InfoCard } from "./Updates.InfoCard";
+
+export function CalendarAndThemingUpdate() {
+  return (
+    <InfoCard title="2022/4/3 功能更新">
+      加入了行事曆查詢功能以及自訂主題。
+    </InfoCard>
+  );
+}
--- src/components/LoginPage/Updates/Updates.tsx
+++ src/components/LoginPage/Updates/Updates.tsx
@@ -1,9 +1,11 @@
+import { CalendarAndThemingUpdate } from "./Updates.CalendarAndTheming";
 import { ScheduleAndNewsUpdate } from "./Updates.ScheduleAndNews";
 import { SecurityUpdate } from "./Updates.Security";
 
 export const Updates = () => {
   return (
     <>
+      <CalendarAndThemingUpdate />
       <ScheduleAndNewsUpdate />
       <SecurityUpdate />
     </>
