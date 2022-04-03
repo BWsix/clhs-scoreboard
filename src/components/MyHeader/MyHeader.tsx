c8a46e88a4b88fb2f56df19c90077f074a33c627
BWsix <itisbw62@gmail.com>
2022-04-03 19:48:08 +0800

    title

--- src/components/MyHeader/MyHeader.tsx
+++ src/components/MyHeader/MyHeader.tsx
@@ -1,4 +1,4 @@
-import { Burger, Header, MediaQuery, Title } from "@mantine/core";
+import { Burger, Group, Header, MediaQuery, Title } from "@mantine/core";
 import { GithubIcon } from "./MyHeader.Github";
 
 interface Props {
@@ -9,25 +9,20 @@ interface Props {
 
 export const MyHeader: React.FC<Props> = ({ opened, toggleSide, noMenu }) => {
   return (
-    <Header height={70} p="md">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        {!noMenu && (
-          <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-            <Burger opened={opened} onClick={toggleSide} size="sm" mr="xl" />
-          </MediaQuery>
-        )}
+    <Header height={70} p="md" sx={{ width: "100%" }}>
+      <Group>
+        <Group spacing="sm" sx={{ flex: 1 }}>
+          {!noMenu && (
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger opened={opened} onClick={toggleSide} size="sm" />
+            </MediaQuery>
+          )}
 
-        <Title order={3}>Scoreboard</Title>
+          <Title order={3}>Scoreboard</Title>
+        </Group>
 
         <GithubIcon />
-      </div>
+      </Group>
     </Header>
   );
 };
