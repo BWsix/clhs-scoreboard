3fe4b0d3efff319e6061bc80abd8999de7bc05f0
BWsix <itisbw62@gmail.com>
2022-04-03 20:20:02 +0800

    added color

--- src/components/MyHeader/MyHeader.Github.tsx
+++ src/components/MyHeader/MyHeader.Github.tsx
@@ -1,14 +1,16 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, useMantineTheme } from "@mantine/core";
 import { BrandGithub } from "tabler-icons-react";
 
 const github = "https://github.com/BWsix/clhs_scoreboard";
 
 export function GithubIcon() {
+  const theme = useMantineTheme();
+
   return (
     <ActionIcon
       size="lg"
       radius="md"
-      variant="filled"
+      color={theme.primaryColor}
       component="a"
       href={github}
       target="_blank"
