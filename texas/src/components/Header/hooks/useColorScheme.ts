8f643c4e77b1a396bea60168b09ee792872aae3b
BWsix <itisbw62@gmail.com>
2022-04-13 23:59:42 +0800

    fix: change default color scheme to light

--- texas/src/components/Header/hooks/useColorScheme.ts
+++ texas/src/components/Header/hooks/useColorScheme.ts
@@ -1,19 +1,13 @@
 import { ColorScheme } from "@mantine/core";
-import {
-  useColorScheme as useSystemColorScheme,
-  useLocalStorage,
-} from "@mantine/hooks";
+import { useLocalStorage } from "@mantine/hooks";
 
 export const useColorScheme = () => {
-  const systemColorScheme = useSystemColorScheme();
-
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
     key: "sb-colorscheme",
-    defaultValue: systemColorScheme,
+    defaultValue: "light",
   });
 
   const toggleColorScheme = (colorScheme?: ColorScheme) => {
-    console.log(colorScheme);
     setColorScheme(
       (prev) => colorScheme || (prev === "dark" ? "light" : "dark")
     );
