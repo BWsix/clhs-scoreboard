bfec232436250e257848d7a9fd31c0f27940956b
BWsix <itisbw62@gmail.com>
2022-04-14 23:38:32 +0800

    update default username to 尚未登入

--- texas/src/components/Navbar/UserName/Username.tsx
+++ texas/src/components/Navbar/UserName/Username.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 
 export const Username = () => {
   const router = useRouter();
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState("尚未登入");
 
   useEffect(() => {
-    setUserName(localStorage.getItem("sb-username") || "");
+    setUserName(localStorage.getItem("sb-username") || "尚未登入");
   }, [router.pathname]);
 
   return <span>{userName}</span>;
