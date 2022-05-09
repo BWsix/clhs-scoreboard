e77068d4c251dd7723267a3cd90c7053951446cf
BWsix <itisbw62@gmail.com>
2022-05-09 08:22:40 +0800

    fix: remove backticks

--- texas/src/components/Login/Updates/Updates.AppIsBroken.tsx
+++ texas/src/components/Login/Updates/Updates.AppIsBroken.tsx
@@ -4,7 +4,7 @@ export function AppIsNowBroken() {
   return (
     <InfoCard title="2022/5/9 悲報">
       學校更新了他們的登入頁面，但進到成績查詢平台後還是沒有更新。總之壢中Scoreboard壞掉了
-      `{">"}`:(
+      {">"}:(
     </InfoCard>
   );
 }
