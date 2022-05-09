0dcc7a1aa1dcd12d668e241c31262fcddc9d8271
BWsix <itisbw62@gmail.com>
2022-05-09 08:13:01 +0800

    update: app is now broken

--- texas/src/components/Login/Updates/Updates.tsx
+++ texas/src/components/Login/Updates/Updates.tsx
@@ -1,8 +1,10 @@
+import { AppIsNowBroken } from "./Updates.AppIsBroken";
 import { UI_Refactor } from "./Updates.UI-Refactor";
 
 export const Updates = () => {
   return (
     <>
+      <AppIsNowBroken />
       <UI_Refactor />
     </>
   );
