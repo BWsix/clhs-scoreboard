4eebee09b753ee35e214793912d899660da4986b
BWsix <itisbw62@gmail.com>
2022-05-13 22:37:49 +0800

    update: unblock login route

--- texas/src/components/Login/Login.tsx
+++ texas/src/components/Login/Login.tsx
@@ -1,11 +1,11 @@
 import { Container } from "@mantine/core";
-// import { LoginPageForm } from "./Login.Form";
+import { LoginPageForm } from "./Login.Form";
 import { Updates } from "./Login.Updates";
 
 export const Login: React.FC = () => {
   return (
     <Container size="xs">
-      {/* <LoginPageForm /> */}
+      <LoginPageForm />
       <Updates />
     </Container>
   );
