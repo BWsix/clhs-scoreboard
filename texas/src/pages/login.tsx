3963fabd4ab7e29d247f6d558504fcc1e76e6264
BWsix <itisbw62@gmail.com>
2022-05-18 14:03:32 +0800

    update: legacy alert

--- texas/src/pages/login.tsx
+++ texas/src/pages/login.tsx
@@ -1,11 +1,16 @@
-import { Alert, Container } from "@mantine/core";
+import { Alert, Container, Text, Title } from "@mantine/core";
 import { IconAlertCircle } from "@tabler/icons";
+import Link from "next/link";
 import { LoginPageForm } from "src/components/Login/Login";
 import { getPageLayout } from "src/layouts/PageLayout";
 
 const INFO_LIST: InfoProps[] = [];
 
 export default function LoginPage() {
+  const isCurrentVersion =
+    typeof window !== "undefined" &&
+    window?.location?.origin === "https://clhs-scoreboard.vercel.app";
+
   const infoList = INFO_LIST.map(({ children, title }, idx) => (
     <InfoCard title={title} key={idx}>
       {children}
@@ -14,6 +19,16 @@ export default function LoginPage() {
 
   return (
     <Container size="xs">
+      {!isCurrentVersion && (
+        <Alert color="red" mb="sm">
+          你正在使用網頁的不是正式版，請使用以下網站以確保你的網站內容得到持續更新：
+          <Link href="http://clhs-scoreboard.vercel.app">
+            <Text sx={{ textDecoration: "underline" }}>
+              http://clhs-scoreboard.vercel.app
+            </Text>
+          </Link>
+        </Alert>
+      )}
       <LoginPageForm />
       {infoList}
     </Container>
