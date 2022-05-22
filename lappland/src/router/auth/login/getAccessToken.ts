1e0cdc9d7536f37d838eb4d11078f33766a36de4
VFLC <itisbw62@gmail.com>
2022-05-22 17:32:42 +0800

    fix: misleading variable name

--- lappland/src/router/auth/login/getAccessToken.ts
+++ lappland/src/router/auth/login/getAccessToken.ts
@@ -12,17 +12,17 @@ export const getAccessToken = async (refreshToken: string) => {
       followRedirect: false,
     });
     const redirectTo = getResult.headers["location"];
-    const accessToken = getResult.headers["set-cookie"]![0]!.split(";")[0];
-    const sessionCookie = `${refreshToken}${accessToken}`;
+    const sessionId = getResult.headers["set-cookie"]![0]!.split(";")[0];
+    const accessToken = `${refreshToken}${sessionId}`;
 
     await got.head(API + redirectTo, {
-      headers: { cookie: sessionCookie },
+      headers: { cookie: accessToken },
       followRedirect: false,
     });
 
     console.log("Finish executing `getAccessToken`: ", Date.now() - t); //TODO remove console.log
 
-    return sessionCookie;
+    return accessToken;
   } catch (e) {
     throw new Error("帳號密碼無誤，但發生錯誤無法登入");
   }
